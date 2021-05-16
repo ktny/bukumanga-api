b8882b0be1e520b45b1f33c1fdf993400dc1ab35
ktny <katsunori.t10@gmail.com>
2021-05-16 16:51:11 +0900

    add created_at, updated_at

--- db/migrations/000001_create_entries_table.up.sql
+++ db/migrations/000001_create_entries_table.up.sql
@@ -1,3 +1,10 @@
+CREATE FUNCTION set_update_time() RETURNS OPAQUE AS '
+  begin
+    new.updated_at := ''now'';
+    return new;
+  end;
+' LANGUAGE plpgsql;
+
 CREATE TABLE IF NOT EXISTS entries(
   id BIGINT PRIMARY KEY,
   title VARCHAR (300) UNIQUE NOT NULL,
@@ -5,5 +12,9 @@ CREATE TABLE IF NOT EXISTS entries(
   site_domain VARCHAR (100) NOT NULL,
   bookmark_count INT NOT NULL,
   image VARCHAR (300),
-  published_at TIMESTAMP NOT NULL
+  published_at TIMESTAMP NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TRIGGER update_tri BEFORE UPDATE ON entries FOR EACH ROW EXECUTE PROCEDURE set_update_time();
