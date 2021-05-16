e418754abd5d469377a0a0c67257827ba823ec3b
ktny <katsunori.t10@gmail.com>
2021-05-16 10:22:03 +0900

    union wait-for-postgres.sh with start.sh

--- Dockerfile
+++ Dockerfile
@@ -30,6 +30,6 @@ RUN echo 'deb http://apt.postgresql.org/pub/repos/apt/ bionic-pgdg main' > /etc/
     apt-get update && apt-get install -y postgresql-client-12
 
 COPY --from=builder /go/bukumanga-api/app /opt/app
-COPY ./scripts/wait-for-postgres.sh ./scripts/start.sh /opt/
+COPY ./scripts/start.sh /opt/
 
 EXPOSE 5000
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+set -euxo pipefail
+
+# wait for postgres
+until PGPASSWORD=${POSTGRES_PASSWORD} psql -h ${POSTGRES_HOST} -U ${POSTGRES_USER} ${POSTGRES_DB} -c '\l'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+>&2 echo "Postgres is up - executing command"
+
+# start app
 migrate -database ${POSTGRESQL_URL} -path db/migrations up
 ./app
