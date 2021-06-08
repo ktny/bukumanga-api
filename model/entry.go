783f6a0390d882eecf0c212ebbb43783ac92de24
ktny <katsunori.t10@gmail.com>
2021-06-08 23:47:24 +0900

    fix

--- model/entry.go
+++ model/entry.go
@@ -3,11 +3,11 @@ package model
 import "database/sql"
 
 type Entry struct {
-	ID int64 `db:"id" json:"id"`
+	ID int32 `db:"id" json:"id"`
 	Title string `db:"title" json:"title"`
 	URL string `db:"url" json:"url"`
 	Domain string `db:"domain" json:"domain"`
-	BookmarkCount int64 `db:"bookmark_count" json:"bookmark_count"`
+	BookmarkCount int16 `db:"bookmark_count" json:"bookmark_count"`
 	Image sql.NullString `db:"image" json:"image"`
 	HotentriedAt string `db:"hotentried_at" json:"hotentried_at"`
 	PublishedAt string `db:"published_at" json:"published_at"`
