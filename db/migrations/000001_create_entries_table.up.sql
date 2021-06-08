25650b58aade4102e7e06903440e2cd93778341c
ktny <katsunori.t10@gmail.com>
2021-06-08 22:58:01 +0900

    add index etc

--- db/migrations/000001_create_entries_table.up.sql
+++ db/migrations/000001_create_entries_table.up.sql
@@ -1,15 +1,21 @@
 CREATE TABLE IF NOT EXISTS entries(
   id SERIAL PRIMARY KEY,
-  title VARCHAR (300) UNIQUE NOT NULL,
+  title VARCHAR (300) NOT NULL,
   url VARCHAR (300) UNIQUE NOT NULL,
   domain VARCHAR (100) NOT NULL,
-  bookmark_count SMALLINT NOT NULL,
+  bookmark_count SMALLINT NOT NULL CHECK (bookmark_count >= 0),
   image VARCHAR (1000),
   hotentried_at DATE NOT NULL,
   published_at DATE NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE INDEX ON entries (title);
+CREATE INDEX ON entries (domain);
+CREATE INDEX ON entries (bookmark_count);
+CREATE INDEX ON entries (hotentried_at);
+CREATE INDEX ON entries (published_at);
 
 CREATE FUNCTION set_update_time() RETURNS TRIGGER AS '
   begin
